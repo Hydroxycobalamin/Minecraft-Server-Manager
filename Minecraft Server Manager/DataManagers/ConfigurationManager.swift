7750637a7959acd5f8ae47d01340c983b8b7c377
Hydroxycobalamin <54799651+Hydroxycobalamin@users.noreply.github.com>
2024-08-26 15:47:14 +0200

    getting the correct path of exec

--- Minecraft Server Manager/DataManagers/ConfigurationManager.swift
+++ Minecraft Server Manager/DataManagers/ConfigurationManager.swift
@@ -3,7 +3,8 @@ import Foundation
 final class ConfigurationManager {
     static let shared = ConfigurationManager()
 
-    let workingFolder = URL(fileURLWithPath: PersistentManager.shared.fileManager.currentDirectoryPath)
+    let workingFolder = URL(fileURLWithPath: CommandLine.arguments[0]).deletingLastPathComponent()
     lazy var downloadDir = workingFolder.appendingPathComponent("downloads")
     lazy var dataFile = workingFolder.appendingPathComponent("data/data.json")
+    lazy var worldDir = workingFolder.appendingPathComponent("worlds")
 }
